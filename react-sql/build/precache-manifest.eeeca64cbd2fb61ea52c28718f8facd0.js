8fffef190fbb0e64ae79ef55e4792c106f03c5dc
sourabh <ranakotisrk@gmail.com>
2020-03-26 23:16:40 +0530

    mod prc

---
+++ react-sql/build/precache-manifest.eeeca64cbd2fb61ea52c28718f8facd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86c23a81c297b7b7f07e828879598b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72ddfca78dfc1be9046e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48dc909abcf44165e675",
+    "url": "/static/js/2.0850d12d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0850d12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ddfca78dfc1be9046e",
+    "url": "/static/js/main.8242afde.chunk.js"
+  },
+  {
+    "revision": "aefb29c750a5f6559749",
+    "url": "/static/js/runtime-main.d438ec90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
